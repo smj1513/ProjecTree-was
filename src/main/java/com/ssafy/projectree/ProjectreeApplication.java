57ffb96de35756c1867fc6faa74960aa89040fc0
Jsplix <jkjin112@gmail.com>
2026-01-16 16:49:05 +0900

    chore: project settings

---
+++ src/main/java/com/ssafy/projectree/ProjectreeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.projectree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectreeApplication.class, args);
+    }
+
+}
